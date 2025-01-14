8b65c6b27b8302a37c4da0ff3b4ccd1385be42b4
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2025-01-14 16:36:17 +0900

    [fix] #12 NisessionDelegate 프로토콜 채택 제거

--- SniffMeet/SniffMeet/Source/LocalNetwork/NI/NIManager.swift
+++ SniffMeet/SniffMeet/Source/LocalNetwork/NI/NIManager.swift
@@ -10,12 +10,8 @@ import MultipeerConnectivity
 import NearbyInteraction
 
 final class NIManager: NSObject {
-    private var niSession: NISession?
+    var niSession: NISession?
     private var cancellables = Set<AnyCancellable>()
-    private let minDistance: Float = 0.09
-    private let maxDistance: Float = 0.15
-    private let minDirection: simd_float3 = simd_float3(-0.6, -0.3, -1.0)
-    private let maxDirection: simd_float3 = simd_float3(0.6, 0.3, -0.8)
 
     @Published var niPaired: Bool = false
     var mpcManager: MPCManager
@@ -32,7 +28,6 @@ final class NIManager: NSObject {
 
     private func setupNISession() {
         niSession = NISession()
-        niSession?.delegate = self
     }
 
     private func setupBindings() {
@@ -111,45 +106,5 @@ final class NIManager: NSObject {
         mpcManager.isAvailableToBeConnected = false
         SNMLogger.log("MPC 세션 종료")
         niPaired = false
-    }
-}
-
-// MARK: - NISessionDelegate
-extension NIManager: NISessionDelegate {
-    func session(_ session: NISession, didUpdate nearbyObjects: [NINearbyObject]) {
-        guard let nearbyObject = nearbyObjects.first else { return }
-        let distance = nearbyObject.distance ?? 1
-        let direction = nearbyObject.direction ?? simd_float3(0.1, 0.1, 0.1)
-
-        SNMLogger.info("Distance and Direction to peer: \(distance) and \(direction)")
-
-        if distance > minDistance && distance < maxDistance {
-            guard let profile = mpcManager.profile else {
-                SNMLogger.log("보낼 데이터가 없다. ")
-                return
-            }
-            SNMLogger.log("거리와 방향 조건 만족")
-            mpcManager.sendData(profile: profile)
-            // 전송 끝
-            // 예측하지 못한 상황
-            Task { @MainActor in
-                isViewTransitioning.send(true) // 메이트리스트에서 화면전환을 해도 된다의 신호
-                viewTransitionInfo.insert("send") // 종료 조건 중 하나를 달성했다. (자신의 프로필 데이터를 보냈다의 의미)
-                // 연결 종료 요청 코드 send
-                mpcManager.send(viewTransitionInfo: "receive") // ni 조건이 만족되기 전에 데이터 전송되니까
-            }
-        }
-    }
-
-    func sessionWasSuspended(_ session: NISession) {
-        SNMLogger.log("NearbyInteraction session suspended.")
-    }
-
-    func sessionSuspensionEnded(_ session: NISession) {
-        SNMLogger.log("NearbyInteraction session suspension ended.")
-    }
-
-    func session(_ session: NISession, didInvalidateWith error: Error) {
-        SNMLogger.error("NearbyInteraction session invalidated: \(error)")
     }
 }

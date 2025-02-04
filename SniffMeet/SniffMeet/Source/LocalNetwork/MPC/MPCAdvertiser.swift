6ca246b7784cd1ee380e439358caaf93c50c5af7
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2025-02-04 15:34:21 +0900

    [refactor] #1 MPCAdvertiser, MPCBrowser 필요없는 코드 제거

--- SniffMeet/SniffMeet/Source/LocalNetwork/MPC/MPCAdvertiser.swift
+++ SniffMeet/SniffMeet/Source/LocalNetwork/MPC/MPCAdvertiser.swift
@@ -4,7 +4,6 @@
 //
 //  Created by 윤지성 on 11/13/24.
 //
-import Combine
 import MultipeerConnectivity
 import os
 
@@ -12,47 +11,23 @@ final class MPCAdvertiser {
     let advertiser: MCNearbyServiceAdvertiser
     let session: MCSession
     let myPeerID: MCPeerID
-
-    static var sharedAdvertiser: MCNearbyServiceAdvertiser?
-
-//    var receivedInvite = PassthroughSubject<Bool, Never>()
-
-    @Published var receivedInviteFrom: MCPeerID?
-    @Published var invitationHandler: ((Bool, MCSession?) -> Void)?
-
+    
     init(advertiser: MCNearbyServiceAdvertiser,
          session: MCSession,
-         myPeerID: MCPeerID,
-         receivedInviteFrom: MCPeerID? = nil,
-         invitationHandler: ((Bool, MCSession?) -> Void)? = nil)
+         myPeerID: MCPeerID)
     {
         self.advertiser = advertiser
         self.session = session
         self.myPeerID = myPeerID
-        self.receivedInviteFrom = receivedInviteFrom
-        self.invitationHandler = invitationHandler
     }
     
     convenience init(session: MCSession, myPeerID: MCPeerID, serviceType: String) {
-        if let existingAdvertiser = MPCAdvertiser.sharedAdvertiser {
-            self.init(advertiser: existingAdvertiser, session: session, myPeerID: myPeerID)
-        } else {
-            let newAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerID,
-                                                          discoveryInfo: nil,
-                                                          serviceType: serviceType)
-            MPCAdvertiser.sharedAdvertiser = newAdvertiser
-            self.init(advertiser: newAdvertiser, session: session, myPeerID: myPeerID)
-            SNMLogger.log("Created new MCNearbyServiceAdvertiser instance")
-        }
+        let newAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerID,
+                                                      discoveryInfo: nil,
+                                                      serviceType: serviceType)
+        self.init(advertiser: newAdvertiser, session: session, myPeerID: myPeerID)
+        SNMLogger.log("Created new MCNearbyServiceAdvertiser instance")
     }
-
-    deinit {
-        if MPCAdvertiser.sharedAdvertiser === advertiser {
-            MPCAdvertiser.sharedAdvertiser = nil
-            SNMLogger.log("MPCAdvertiser deinit")
-        }
-    }
-
     func startAdvertising() {
         advertiser.startAdvertisingPeer()
         SNMLogger.log("start advertising")
@@ -60,7 +35,6 @@ final class MPCAdvertiser {
     
     func stopAdvertising() {
         advertiser.stopAdvertisingPeer()
-//        receivedInvite.send(false)
         SNMLogger.log("stop advertising")
     }
 }
--- SniffMeet/SniffMeet/Source/LocalNetwork/MPC/MPCBroswer.swift
+++ SniffMeet/SniffMeet/Source/LocalNetwork/MPC/MPCBroswer.swift
@@ -11,10 +11,7 @@ final class MPCBrowser {
     let browser: MCNearbyServiceBrowser
     let session: MCSession
     let myPeerId: MCPeerID
-
-    static var sharedBrowser: MCNearbyServiceBrowser?
-
-
+    
     init(browser: MCNearbyServiceBrowser,
          session: MCSession,
          myPeerId: MCPeerID)
@@ -22,43 +19,25 @@ final class MPCBrowser {
         self.browser = browser
         self.session = session
         self.myPeerId = myPeerId
-
+    }
+    convenience init(session: MCSession, myPeerID: MCPeerID, serviceType: String) {
+        let newBrowser = MCNearbyServiceBrowser(peer: myPeerID,
+                                                serviceType: serviceType)
+        self.init(browser: newBrowser, session: session, myPeerId: myPeerID)
     }
     
-    convenience init(session: MCSession, myPeerID: MCPeerID, serviceType: String) {
-        if let existingBrowser = MPCBrowser.sharedBrowser {
-            self.init(browser: existingBrowser, session: session, myPeerId: myPeerID)
-        } else {
-            let newBrowser = MCNearbyServiceBrowser(peer: myPeerID,
-                                                    serviceType: serviceType)
-            MPCBrowser.sharedBrowser = newBrowser
-            self.init(browser: newBrowser, session: session, myPeerId: myPeerID)
-        }
-    }
-
-    deinit {
-        if MPCBrowser.sharedBrowser === browser {
-            MPCBrowser.sharedBrowser = nil
-            SNMLogger.log("MPCBrowser deinit")
-        }
-    }
-
     func startBrowsing() {
         browser.startBrowsingForPeers()
         SNMLogger.log("start Browsing")
-
     }
-    
     func stopBrowsing() {
         browser.stopBrowsingForPeers()
         SNMLogger.log("stop Browsing")
     }
-
     func invite(peerID: MCPeerID) {
         browser.invitePeer(peerID, to: session, withContext: nil, timeout: 30)
         SNMLogger.log("invitePeer peerID")
     }
-    
     func invite(peerID: MCPeerID, tokenData: Data) {
         browser.invitePeer(peerID, to: session, withContext: tokenData, timeout: 30)
         SNMLogger.log("invitePeer tokenData")

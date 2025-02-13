b67f2816afd4177b1918fc5a81889c813448b17b
pearhyunjin <pearhyunjin@gmail.com>
2025-02-13 13:21:54 +0900

    [feat] #12 NI에서 지원하는 isSupported 이용해 사용 가능 판단하도록 변경

--- SniffMeet/SniffMeet/Source/Mate/AddMate/Interactor/ProfileDropInteractor.swift
+++ SniffMeet/SniffMeet/Source/Mate/AddMate/Interactor/ProfileDropInteractor.swift
@@ -20,18 +20,18 @@ final class ProfileDropInteractor: ProfileDropInteractable {
     private var tryProfileDropUseCase: any TryProfileDropUseCase
     private var quitProfileDropUseCase: any QuitProfileDropUseCase
     private var cancellables: Set<AnyCancellable> = []
-    private let deviceInfoFinder: DeviceInfoFinderProtocol
+    private let niDeviceChecker: NIDeviceCheckerProtocol
 
     init(
         presenter: ProfileDropInteractorOutput? = nil,
         tryProfileDropUseCase: any TryProfileDropUseCase,
         quitProfileDropUseCase: any QuitProfileDropUseCase,
-        deviceInfoFinder: DeviceInfoFinderProtocol
+        niDeviceChecker: NIDeviceCheckerProtocol
     ) {
         self.presenter = presenter
         self.tryProfileDropUseCase = tryProfileDropUseCase
         self.quitProfileDropUseCase = quitProfileDropUseCase
-        self.deviceInfoFinder = deviceInfoFinder
+        self.niDeviceChecker = niDeviceChecker
 
         bind()
     }
@@ -76,9 +76,8 @@ final class ProfileDropInteractor: ProfileDropInteractable {
     }
 
     func checkNISupport() {
-        let isSupportedNI = deviceInfoFinder.isDeviceSupportedNI()
-        SNMLogger.log("isSupportedNI: \(isSupportedNI)")
-        if isSupportedNI == false {
+        let isSupported = niDeviceChecker.isNISupported()
+        if isSupported == false {
             presenter?.updateDeviceInfo()
         }
     }
--- SniffMeet/SniffMeet/Source/Mate/AddMate/Router/ProfileDropRouter.swift
+++ SniffMeet/SniffMeet/Source/Mate/AddMate/Router/ProfileDropRouter.swift
@@ -58,12 +58,12 @@ extension ProfileDropRouter: ProfileDropBuildable {
             mpcManager: mpcManager)
         let quitProfileDropUseCase: QuitProfileDropUseCase =
         QuitProfileDropUseCaseImpl(niManager: niManager)
-        let deviceInfoFinder: DeviceInfoFinderProtocol = DeviceInfoFinder()
+        let niDeviceChecker: NIDeviceCheckerProtocol = NIDeviceChecker()
 
         let view: ProfileDropViewable & UIViewController = ProfileDropViewController()
         let router: ProfileDropRoutable & ProfileDropBuildable = ProfileDropRouter()
         let presenter: ProfileDropPresentable & ProfileDropInteractorOutput = ProfileDropPresenter()
-        let interactor: ProfileDropInteractable = ProfileDropInteractor(tryProfileDropUseCase: tryProfileDropUseCase, quitProfileDropUseCase: quitProfileDropUseCase, deviceInfoFinder: deviceInfoFinder)
+        let interactor: ProfileDropInteractable = ProfileDropInteractor(tryProfileDropUseCase: tryProfileDropUseCase, quitProfileDropUseCase: quitProfileDropUseCase, niDeviceChecker: niDeviceChecker)
 
         view.presenter = presenter
         presenter.view = view

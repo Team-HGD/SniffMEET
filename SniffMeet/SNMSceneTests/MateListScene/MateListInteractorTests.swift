1702e381f76c34352380f915fa561f5955a6cae0
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2025-02-16 12:13:09 +0900

    [fix] 테스트 주석처리

--- SniffMeet/SNMSceneTests/MateListScene/MateListInteractorTests.swift
+++ SniffMeet/SNMSceneTests/MateListScene/MateListInteractorTests.swift
@@ -32,8 +32,6 @@ final class MateListInteractorTests: XCTestCase {
             presenter: presenterSpy,
             requestMateListUseCase: requestMateListUseCaseMock,
             requestProfileImageUseCase: requestProfileImageUseCaseMock,
-            tryProfileDropUseCase: tryProfileDropUseCaseMock,
-            quitProfileDropUseCase: quitProfileDropUseCaseMock,
             deleteMateUseCase: deleteMateUseCaseMock
         )
     }
@@ -47,41 +45,41 @@ final class MateListInteractorTests: XCTestCase {
         quitProfileDropUseCaseMock = nil
     }
     
-    func test_tryProfileDropUseCase가_profilePublisher를_send_호출시_presenter에_프로필데이터를_전달한다() async throws {
-        // Arrange
-        let receivedProfile = DogDTO(id: UUID(), name: "lemon", keywords: [.shy], profileImage: nil)
-        // Act
-        tryProfileDropUseCaseMock.profilePublisher.send(receivedProfile)
-        // Assert
-        try await Task.sleep(nanoseconds: 1000000000)
-        XCTAssertTrue(presenterSpy.presentProfileDataCalled,
-                      "presenter가 프로필 드랍 데이터를 처리하는 메서드를 호출한다." )
-        if let receivedId = presenterSpy.receivedProfileData?.id {
-            XCTAssertEqual(receivedId, receivedProfile.id)
-        } else {
-            XCTFail("프로필 데이터를 받지 못했다. ")
-        }
-    }
-    func test_tryProfileDropUseCase가_NI세션이_연결되면_presenter에_연결사실을_알린다() async throws {
-        // Arrange
-        // Act
-        tryProfileDropUseCaseMock.isNIConnected.send(true)
-        try await Task.sleep(nanoseconds: 1000000000)
-        
-        // Assert
-            XCTAssertTrue(presenterSpy.presentNIConnectedCalled,
-                          "presenter가 NI 연결 상태를 처리하는 메서드를 호출한다." )
-
-    }
-    func test_tryProfileDropUseCase가_NI세션이_연결이_종료되면_presenter에_연결이_끊긴_사실을_알린다() async throws {
-        // Arrange
-        let expectation = XCTestExpectation(description: "Presenter processes NI Not Connected")
-        // Act
-        tryProfileDropUseCaseMock.isNIConnected.send(false)
-        try await Task.sleep(nanoseconds: 1000000000)
-
-        // Assert
-        XCTAssertTrue(presenterSpy.presentNINotConnectedCalled,
-                      "presenter가 NI 연결이 끊긴 상태를 처리하는 메서드를 호출한다." )
-    }
+//    func test_tryProfileDropUseCase가_profilePublisher를_send_호출시_presenter에_프로필데이터를_전달한다() async throws {
+//        // Arrange
+//        let receivedProfile = DogDTO(id: UUID(), name: "lemon", keywords: [.shy], profileImage: nil)
+//        // Act
+//        tryProfileDropUseCaseMock.profilePublisher.send(receivedProfile)
+//        // Assert
+//        try await Task.sleep(nanoseconds: 1000000000)
+//        XCTAssertTrue(presenterSpy.presentProfileDataCalled,
+//                      "presenter가 프로필 드랍 데이터를 처리하는 메서드를 호출한다." )
+//        if let receivedId = presenterSpy.receivedProfileData?.id {
+//            XCTAssertEqual(receivedId, receivedProfile.id)
+//        } else {
+//            XCTFail("프로필 데이터를 받지 못했다. ")
+//        }
+//    }
+//    func test_tryProfileDropUseCase가_NI세션이_연결되면_presenter에_연결사실을_알린다() async throws {
+//        // Arrange
+//        // Act
+//        tryProfileDropUseCaseMock.isNIConnected.send(true)
+//        try await Task.sleep(nanoseconds: 1000000000)
+//        
+//        // Assert
+//            XCTAssertTrue(presenterSpy.presentNIConnectedCalled,
+//                          "presenter가 NI 연결 상태를 처리하는 메서드를 호출한다." )
+//
+//    }
+//    func test_tryProfileDropUseCase가_NI세션이_연결이_종료되면_presenter에_연결이_끊긴_사실을_알린다() async throws {
+//        // Arrange
+//        let expectation = XCTestExpectation(description: "Presenter processes NI Not Connected")
+//        // Act
+//        tryProfileDropUseCaseMock.isNIConnected.send(false)
+//        try await Task.sleep(nanoseconds: 1000000000)
+//
+//        // Assert
+//        XCTAssertTrue(presenterSpy.presentNINotConnectedCalled,
+//                      "presenter가 NI 연결이 끊긴 상태를 처리하는 메서드를 호출한다." )
+//    }
 }

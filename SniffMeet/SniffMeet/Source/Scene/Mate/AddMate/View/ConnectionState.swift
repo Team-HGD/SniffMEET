5b21dee1fd157584c7efa82a7e7cb9013a797483
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2025-02-18 00:21:47 +0900

    [feat] #27 connectionState 세분화
    NI 있는 ver, 없는 ver에 따라 에니메이션이 다르게 보여야 할 거 같아서 state를 세분화했습니다.

--- SniffMeet/SniffMeet/Source/Scene/Mate/AddMate/View/ConnectionState.swift
+++ SniffMeet/SniffMeet/Source/Scene/Mate/AddMate/View/ConnectionState.swift
@@ -7,15 +7,18 @@
 
 enum ConnectionState {
     case connecting
-    case success
+    case successNISession
+    case successMPCSession
     case failure
 
     var description: String {
         switch self {
         case .connecting:
             return "연결 중입니다."
-        case .success:
+        case .successNISession:
             return "연결 성공, 프로필 드랍 시도하세요"
+        case .successMPCSession:
+            return "프로필 전송 버튼을 눌러 프로필을 공유해보세요"
         case .failure:
             return "연결 실패, 다시 시도하세요"
         }

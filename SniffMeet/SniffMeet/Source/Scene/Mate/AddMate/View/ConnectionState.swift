1a91e3df007b64738cdc5222f4c386955dd906ee
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2025-02-18 00:27:53 +0900

    [fix] #27 connectionState에 따른 멘트 수정

--- SniffMeet/SniffMeet/Source/Scene/Mate/AddMate/View/ConnectionState.swift
+++ SniffMeet/SniffMeet/Source/Scene/Mate/AddMate/View/ConnectionState.swift
@@ -16,9 +16,9 @@ enum ConnectionState {
         case .connecting:
             return "연결 중입니다."
         case .successNISession:
-            return "연결 성공, 프로필 드랍 시도하세요"
+            return "연결 성공, 서로 기기를 위와 같이 움직여보세요."
         case .successMPCSession:
-            return "프로필 전송 버튼을 눌러 프로필을 공유해보세요"
+            return "연결 성공, 친구가 프로필을 보내고 있어요."
         case .failure:
             return "연결 실패, 다시 시도하세요"
         }

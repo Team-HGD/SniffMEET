a04a712038404be938224d9815832913a5bb8c08
pearhyunjin <pearhyunjin@gmail.com>
2025-02-21 08:40:07 +0900

    [feat] #37 실시간 산책 기록 저장할 WalkRecord 추가

--- SniffMeet/SniffMeet/Source/Entity/WalkLog.swift
+++ SniffMeet/SniffMeet/Source/Entity/WalkLog.swift
@@ -18,3 +18,23 @@ struct WalkLog {
         endDate.timeIntervalSince(startDate)
     }
 }
+
+struct WalkRecord {
+    let stepCount: Int
+    let distance: Double
+    let formattedTime: String
+
+    init(stepCount: Int, totalDistance: Double, time: TimeInterval) {
+        self.stepCount = stepCount
+        self.distance = round(totalDistance * 100) / 100
+        self.formattedTime = WalkRecord.formatTime(time)
+    }
+
+    private static func formatTime(_ timeInterval: TimeInterval) -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.zeroFormattingBehavior = .pad
+        formatter.unitsStyle = .positional
+        return formatter.string(from: timeInterval) ?? "00:00:00"
+    }
+}

aa8c806b580dfcf0353fa2bd221b89c84262e022
pearhyunjin <pearhyunjin@gmail.com>
2025-02-27 09:14:48 +0900

    [feat] #55 뷰 레이아웃 잡기

--- SniffMeet/SniffMeet/Source/Scene/Auth/SignIn/View/SigninViewController.swift
+++ SniffMeet/SniffMeet/Source/Scene/Auth/SignIn/View/SigninViewController.swift
@@ -13,18 +13,94 @@ protocol SigninViewable: AnyObject {
 
 final class SigninViewController: BaseViewController, SigninViewable {
     var presenter: (any SigninPresentable)?
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = Context.title
+        label.textColor = SNMColor.mainNavy
+        label.font = SNMFont.bigLogoTitle
+        return label
+    }()
+    private var idTextField: InputTextField = InputTextField(placeholder: Context.idPlaceholder)
+    private var pwTextField: InputTextField = InputTextField(placeholder: Context.pwPlaceholder)
+    private var signInButton = PrimaryButton(title: Context.signinButtonTitle)
+    private var signUpLabel: UILabel = {
+        let label = UILabel()
+        label.text = Context.signupButtonTitle
+        label.textColor = SNMColor.mainNavy
+        label.font = SNMFont.body
+        return label
+    }()
+    private var findPWLabel: UILabel = {
+        let label = UILabel()
+        label.text = Context.findPWButtonTitle
+        label.textColor = SNMColor.mainNavy
+        label.font = SNMFont.body
+        return label
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func configureAttributes() {
+        hideKeyboardWhenTappedAround()
     }
 
     override func configureHierachy() {
+        [titleLabel,
+         idTextField,
+         pwTextField,
+         signInButton,
+         signUpLabel,
+         findPWLabel].forEach{
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
     }
 
     override func configureConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: Context.firstVerticalPadding),
+            idTextField.topAnchor.constraint(
+                equalTo: titleLabel.bottomAnchor,
+                constant: LayoutConstant.largeVerticalPadding * 2),
+            idTextField.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: LayoutConstant.horizontalPadding),
+            idTextField.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -LayoutConstant.horizontalPadding),
+            pwTextField.topAnchor.constraint(
+                equalTo: idTextField.bottomAnchor,
+                constant: LayoutConstant.regularVerticalPadding),
+            pwTextField.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: LayoutConstant.horizontalPadding),
+            pwTextField.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -LayoutConstant.horizontalPadding),
+            signInButton.topAnchor.constraint(
+                equalTo: pwTextField.bottomAnchor,
+                constant: LayoutConstant.xlargeVerticalPadding),
+            signInButton.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: LayoutConstant.horizontalPadding),
+            signInButton.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -LayoutConstant.horizontalPadding),
+            signUpLabel.topAnchor.constraint(
+                equalTo: signInButton.bottomAnchor,
+                constant: LayoutConstant.xlargeVerticalPadding),
+            signUpLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            findPWLabel.topAnchor.constraint(
+                equalTo: signUpLabel.bottomAnchor,
+                constant: LayoutConstant.regularVerticalPadding),
+            findPWLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 
     override func bind() {
@@ -33,5 +109,12 @@ final class SigninViewController: BaseViewController, SigninViewable {
 
 private extension SigninViewController {
     enum Context {
+        static let title = "SniffMEET"
+        static let signinButtonTitle = "로그인"
+        static let signupButtonTitle = "회원가입"
+        static let findPWButtonTitle = "비밀번호 찾기"
+        static let idPlaceholder: String = "아이디를 입력해주세요."
+        static let pwPlaceholder: String = "비밀번호를 입력해주세요."
+        static let firstVerticalPadding: CGFloat = 100
     }
 }

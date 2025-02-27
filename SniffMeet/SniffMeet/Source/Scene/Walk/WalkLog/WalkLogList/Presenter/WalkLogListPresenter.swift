7ceb4a426dde89973e9350c6d045157a8077f1c3
Hyemin Heo <heohm128@gmail.com>
2025-02-27 19:31:26 +0900

    [feat] #58 presenter와 interactor 연결

--- SniffMeet/SniffMeet/Source/Scene/Walk/WalkLog/WalkLogList/Presenter/WalkLogListPresenter.swift
+++ SniffMeet/SniffMeet/Source/Scene/Walk/WalkLog/WalkLogList/Presenter/WalkLogListPresenter.swift
@@ -5,14 +5,61 @@
 //  Created by sole on 2/27/25.
 //
 
+import Combine
+import Foundation
+
 protocol WalkLogListPresentable: AnyObject {
     var view: (any WalkLogListViewable)? { get }
     var interactor: (any WalkLogListInteractable)? { get }
     var router: (any WalkLogListRoutable)? { get }
+    var output: (any WalkLogListPresenterOutput) { get }
+
+    func viewDidLoad()
 }
 
 final class WalkLogListPresenter: WalkLogListPresentable {
     weak var view: (any WalkLogListViewable)?
     var interactor: (any WalkLogListInteractable)?
     var router: (any WalkLogListRoutable)?
+    var output: any WalkLogListPresenterOutput
+
+    init(
+        view: (any WalkLogListViewable)? = nil,
+        interactor: (any WalkLogListInteractable)? = nil,
+        router: (any WalkLogListRoutable)? = nil,
+        output: any WalkLogListPresenterOutput = DefaultWalkLogListPresenterOutput(
+            walkLogList: [],
+            name: "후추추",
+            profileImageData: nil
+        )
+    ) {
+        self.view = view
+        self.interactor = interactor
+        self.router = router
+        self.output = output
+    }
+
+    func viewDidLoad() {
+        do {
+            let (name, profileImageData) = try interactor?.fetchUserInfo() ?? ("", nil)
+            output.name = name
+            output.profileImageData = profileImageData
+            let walkLogList = try interactor?.fetchWalkLogList()
+            output.walkLogList = walkLogList ?? []
+        } catch {
+            SNMLogger.log(error.localizedDescription)
+        }
+    }
+}
+
+protocol WalkLogListPresenterOutput {
+    var walkLogList: [WalkLog] { get set }
+    var name: String { get set }
+    var profileImageData: Data? { get set }
+}
+
+struct DefaultWalkLogListPresenterOutput: WalkLogListPresenterOutput {
+    var walkLogList: [WalkLog]
+    var name: String
+    var profileImageData: Data?
 }

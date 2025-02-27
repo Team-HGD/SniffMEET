de6f4b4c3529162c60f457ff13129aaeb9754553
Hyemin Heo <heohm128@gmail.com>
2025-02-27 19:31:43 +0900

    [feat] #58 presenter와 연결

--- SniffMeet/SniffMeet/Source/Scene/Walk/WalkLog/WalkLogList/View/WalkLogListViewController.swift
+++ SniffMeet/SniffMeet/Source/Scene/Walk/WalkLog/WalkLogList/View/WalkLogListViewController.swift
@@ -15,10 +15,16 @@ final class WalkLogListViewController: BaseViewController, WalkLogListViewable {
     var presenter: (any WalkLogListPresentable)?
     private let walkLogTableView: UITableView = UITableView()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter?.viewDidLoad()
+    }
+
     override func configureAttributes() {
         walkLogTableView.dataSource = self
         walkLogTableView.delegate = self
         walkLogTableView.separatorInset = .zero
+        walkLogTableView.allowsSelection = false
     }
     override func configureHierachy() {
         [walkLogTableView].forEach {
@@ -34,6 +40,23 @@ final class WalkLogListViewController: BaseViewController, WalkLogListViewable {
             walkLogTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
+
+    private func distanceLabelString(_ meters: Double) -> String {
+        String(format: "%.2f km", meters / 1000)
+    }
+    private func dateLabelString(_ date: Date) -> String {
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        return dateFormatter.string(from: date)
+    }
+    private func durationLabelString(_ timeInterval: TimeInterval) -> String {
+        let totalSeconds = Int(timeInterval)
+        let hours = totalSeconds / (60 * 60)
+        let minutes = (totalSeconds / 60) % 60
+        let seconds = Int(totalSeconds) % 60
+
+        return hours > 0 ? "\(hours)시간 \(minutes)분" : "\(minutes)분 \(seconds)초"
+    }
 }
 
 //MARK: - WalkLogListViewController+UITableView DataSource, Delegate
@@ -43,14 +66,34 @@ extension WalkLogListViewController: UITableViewDataSource {
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int {
-        3
+        presenter?.output.walkLogList.count ?? 0
     }
     
     func tableView(
         _ tableView: UITableView,
         cellForRowAt indexPath: IndexPath
     ) -> UITableViewCell {
-        let cell = WalkLogCell(style: .default, reuseIdentifier: WalkLogCell.identifier)
+        let cell: WalkLogCell = walkLogTableView.dequeueReusableCell(
+            withIdentifier: WalkLogCell.identifier
+        ) as? WalkLogCell ?? WalkLogCell(style: .default, reuseIdentifier: WalkLogCell.identifier)
+        let walkLogs: [WalkLog] = presenter?.output.walkLogList ?? []
+
+        cell.configureWalkLogSection(
+            date: dateLabelString(walkLogs[indexPath.row].endDate),
+            distance: distanceLabelString(walkLogs[indexPath.row].distance),
+            step: "\(walkLogs[indexPath.row].step) 걸음",
+            duration: durationLabelString(walkLogs[indexPath.row].duration),
+            image: UIImage(
+                data: walkLogs[indexPath.row].image ?? Data()
+            )
+        )
+        cell.configureProfileSection(
+            profileImage: UIImage(
+                data: presenter?.output.profileImageData ?? Data()
+            ),
+            name: presenter?.output.name ?? "후추추"
+        )
+
         return cell
     }
 }
@@ -61,11 +104,5 @@ extension WalkLogListViewController: UITableViewDelegate {
         heightForRowAt indexPath: IndexPath
     ) -> CGFloat {
         393
-    }
-    func tableView(
-        _ tableView: UITableView,
-        didSelectRowAt indexPath: IndexPath
-    ) {
-        tableView.deselectRow(at: indexPath, animated: true)
     }
 }

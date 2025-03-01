d8c231f6c64804371463fd6610e8257f1817432e
Jinwon Choi <jjwls97@catholic.ac.kr>
2025-03-01 20:02:50 +0900

    [fix] #54 유저 정보 업데이트 유즈케이스 수정
    - 서버에 요청을 할 때, DTO 객체를 새로 만들어서 요청하지 않고, 필요한 어트리뷰트만 수정할 수 있도록 쿼리 바디를 구성하도록 수정했습니다.
    - 마찬가지로 로컬과 서버의 무결성을 보장하도록 작성했습니다.

--- SniffMeet/SniffMeet/Source/UseCase/SaveUseCase/UpdateUserInfoUseCase.swift
+++ SniffMeet/SniffMeet/Source/UseCase/SaveUseCase/UpdateUserInfoUseCase.swift
@@ -8,32 +8,67 @@
 import Foundation
 
 protocol UpdateUserInfoUseCase {
-    func execute(info: UserInfoDTO) async
+    func execute(with updatedProperty: [String: Any]) async throws
 }
 
 struct UpdateUserInfoUseCaseImpl: UpdateUserInfoUseCase {
+    private let localDBManager: any UserDefaultsManagable
     private let remoteDBManager: any RemoteDBManageable
     private let sessionManager: any SessionManageable
     
     init(
+        localDBManager: any UserDefaultsManagable,
         remoteDBManager: any RemoteDBManageable,
         sessionManager: any SessionManageable
     ) {
+        self.localDBManager = localDBManager
         self.remoteDBManager = remoteDBManager
         self.sessionManager = sessionManager
     }
     
-    func execute(info: UserInfoDTO) async {
+    func execute(with updatedProperty: [String: Any]) async throws {
         do {
-            let userID = try sessionManager.userID.get()
-            let userData = try JSONEncoder().encode(info)
-            try await remoteDBManager.updateData()
-                .setTable(Environment.SupabaseTableName.userInfo)
-                .setData(userData)
-                .setQuery(.equal("id", userID))
-                .request()
-        } catch {
-            SNMLogger.error("\(error.localizedDescription)")
+            try updateToLocal(with: updatedProperty)
+            try await updateToRemote(with: updatedProperty)
+        } catch let error as UserDefaultsError {
+            throw SNMError(level: .user, error: error)
+        } catch let error as SupabaseSessionError {
+            try localDBManager.delete(forKey: Environment.UserDefaultsKey.dogInfo)
+            throw SNMError(level: .user, error: error)
+        } catch let error as SupabaseDBError {
+            try localDBManager.delete(forKey: Environment.UserDefaultsKey.dogInfo)
+            throw SNMError(level: .user, error: error)
         }
+    }
+    
+    private func updateToLocal(with updatedProperty: [String: Any]) throws {
+        let oldProfileInfo = try localDBManager.get(
+            forKey: Environment.UserDefaultsKey.dogInfo,
+            type: ProfileInfo.self
+        )
+        let newProfileInfo = ProfileInfo(
+            name: updatedProperty["name"] as? String ?? oldProfileInfo.name,
+            age: updatedProperty["age"] as? UInt8 ?? oldProfileInfo.age,
+            sex: oldProfileInfo.sex,
+            sexUponIntake: updatedProperty["sexUponIntake"] as? Bool ?? oldProfileInfo.sexUponIntake,
+            size: (updatedProperty["size"] as? String).flatMap { sizeString in
+                Size(rawValue: sizeString)
+            } ?? oldProfileInfo.size,
+            keywords: (updatedProperty["keywords"] as? [String])?.compactMap { keywordString in
+                Keyword(rawValue: keywordString)
+            } ?? oldProfileInfo.keywords,
+            nickname: updatedProperty["nickname"] as? String ?? oldProfileInfo.nickname,
+            profileImageName: oldProfileInfo.profileImageName
+        )
+        try localDBManager.set(value: newProfileInfo, forKey: Environment.UserDefaultsKey.dogInfo)
+    }
+    private func updateToRemote(with updatedProperty: [String: Any]) async throws {
+        let userID = try sessionManager.userID.get()
+        let data = try JSONSerialization.data(withJSONObject: updatedProperty, options: [])
+        try await remoteDBManager.updateData()
+            .setTable(Environment.SupabaseTableName.userInfo)
+            .setData(data)
+            .setQuery(.equal("id", userID))
+            .request()
     }
 }
